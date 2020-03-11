ab87e8d2bc9afeeb65b562594895a2b48b4cc3cb
Denzaaaaal <denzeldouglas@protonmail.com>
2020-03-11 17:57:41 +0000

    completed joining table query

--- database/selecting_data.sql
+++ database/selecting_data.sql
@@ -1,2 +1,8 @@
-/* Selecting income ammount for  */
-select username, first_name, last_name, ammount from user_details, income_1 where user_details.id = usr_id and usr_id = 1;
+/* Selecting all income amounts for a user  */
+select username, first_name, last_name, amount from user_details, income_1 where user_details.id = usr_id and usr_id = 1;
+
+/* Selecting all expense amount for a user*/
+select username, first_name, last_name, amount from user_details, expense where user_details.id = user_id and user_id = 1;
+
+/* Selecting all investments for a user */
+select username, first_name, last_name, amount from user_details, investment where userdetails.id = user_id and userid = 1;
