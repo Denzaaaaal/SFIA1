3446294f6481aed2a696e32756425c27174ff628
Denzaaaaal <denzeldouglas@protonmail.com>
2020-03-23 00:29:05 +0000

    added python3-flask package

--- scripts/before_installation.sh
+++ scripts/before_installation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Installing Dependencies..."
-sudo apt update -y -qq
-sudo apt install python3 -y -qq
-sudo apt install python3-pip -y -qq
-sudo apt install python3-venv -y -qq
-sudo apt-get install libmysqlclient-dev -y -qq
-sudo apt-get install gunicorn -y -qq
+sudo apt update -y -qq > /dev/null
+sudo apt install python3 -y -qq > /dev/null
+sudo apt install python3-pip -y -qq > /dev/null
+sudo apt install python3-flask -y -qq > /dev/null
+sudo apt install python3-venv -y -qq > /dev/null
+sudo apt-get install libmysqlclient-dev -y -qq > /dev/null
+sudo apt-get install gunicorn -y -qq > /dev/null
 python3 -m venv venv
