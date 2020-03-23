0bb73256d519629f9f35185ce0e6288a099ff7e2
Denzaaaaal <denzeldouglas@protonmail.com>
2020-03-23 00:21:01 +0000

    added gunicorn

--- scripts/before_installation.sh
+++ scripts/before_installation.sh
@@ -5,4 +5,5 @@ sudo apt install python3 -y -qq
 sudo apt install python3-pip -y -qq
 sudo apt install python3-venv -y -qq
 sudo apt-get install libmysqlclient-dev -y -qq
+sudo apt-get install gunicorn -y -qq
 python3 -m venv venv
